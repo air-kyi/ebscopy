9689050d26d5ba3a02179b7a2a8ac102fe65bd5b
air-kyi <113389784+air-kyi@users.noreply.github.com>
2023-06-05 14:10:59 -0700

    remove restriction on python version 3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
 	include_package_data	= True,
 	description				= 'Official Python wrapper for the EBSCO Discovery Service (EDS) API',
 	long_description		= ld,
-	python_requires			= '>=2.7,!=3.*,',
+	python_requires			= '>=2.7',
 	install_requires		= ["requests"],
 	data_files				= [ 
 								("ebscopy/samples", 
